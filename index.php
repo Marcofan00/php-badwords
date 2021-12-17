babdf325ece6d1e70299185d5ef64bdb9d8981c6
Marco <marcofan00@gmail.com>
2021-12-17 15:51:29 +0100

    primo ex

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ex PHP</title>
+</head>
+<body>
+
+<?php
+      $a= "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam, tempore quasi aliquid beatae commodi hic laboriosam quam, nam sunt cumque eaque ratione at vel, possimus accusamus pariatur. Velit, adipisci architecto.";
+    ?>
+    <h1> <?php echo $a ?></h1>
+
+    <p><?php echo "la lunghezza del paragrafo  è: " . strlen($a) ?> </p>
+</body>
+</html><?php
+
+?>
